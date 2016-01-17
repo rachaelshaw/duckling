15a5db59ef9a82a99850b7d44298f3a407a25f50
Irl Nathan <irlnathan@gmail.com>
2016-01-17 15:24:24 -0600

    additional formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,6 @@ angular.module('brushfire', ['toastr', 'compareTo', 'ui.bootstrap'])
 });
 
 angular.module('brushfire').run(['$http', function($http) {
-  
   if (window.SAILS_LOCALS._csrf) {
     $http.defaults.headers.common['X-CSRF-Token'] = window.SAILS_LOCALS._csrf;
   }
